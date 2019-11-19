a12b64171022576536d5afa86fc64b23d5c11023
panayiotask <panayiotasketsiou@gmail.com>
2019-11-19 15:30:49 +0000

    ex01

---
+++ ex01.py
@@ -0,0 +1,20 @@
+# 1
+course = 'python'
+rating = 10
+print(course, rating)
+
+# 2
+b=3
+c=4
+a=(b**2+c**2)**1/2
+print(a)
+
+# 3
+print(a)
+print(b)
+print(c)
+
+# 4
+a2=int(a)
+print(a2)
+print(str(a2)+' squared equals ' + str(b) + ' squared + ' + str(c) + ' squared.')
