69f86fe812d4837f65fa6311fa2ba8504defccfc
panayiotask <panayiotasketsiou@gmail.com>
2019-11-20 16:43:06 +0000

    ex09

---
+++ ex09.py
@@ -0,0 +1,14 @@
+# 1
+# Ran in Linux shell:
+# $ mkdir dancing
+# $ cd dancing/
+# $ gedit __init__.py
+# $ cd ..
+# $ python -c "import dancing"
+# $ python -c "import dancing; print(dancing.v)"
+
+# 2
+# Ran in Linux shell
+
+# 3
+# created dancer.py
