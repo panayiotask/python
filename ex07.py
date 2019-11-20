de50ccd3e5ac9f5a89131b39f1cbffddaa3905a8
panayiotask <panayiotasketsiou@gmail.com>
2019-11-20 14:43:28 +0000

    ex07

---
+++ ex07.py
@@ -0,0 +1,31 @@
+# 1
+a=[0,1,2]
+b=a
+print(a)
+print(b)
+b[0]='hello'
+print(a)
+print(b)
+a.append('3')
+print(a)
+print(b)
+
+# 2
+a=('can I change')
+b=a
+print(a)
+print(b)
+b=('different')
+print(a)
+print(b)
+
+# 3
+a=[0,1,2]
+import copy
+b=copy.deepcopy(a)
+print(a)
+print(b)
+b[0]='hello'
+print(a)
+print(b)
+
